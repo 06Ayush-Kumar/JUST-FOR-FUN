c178768a1b5daf2d10c3a9737ac186f1794a5c2e
Cyrus_The_Great <166158964+06Ayush-Kumar@users.noreply.github.com>
2024-10-14 19:09:44 +0530

    Update Leaf Dynamics.cpp

--- Leaf Dynamics.cpp
+++ Leaf Dynamics.cpp
@@ -17,16 +17,60 @@ private:
 
 public:
     
-    Leaf(float ws, float m, float a, float af) 
-        : wind_speed(ws), mass(m), area(a), attach_force(af) {}
-
-    
-    float calculateDragForce() {
-        return 0.5 * Cd * density * area * wind_speed * wind_speed;
+    Leaf(float ws, float m, float a, float af) {
+        check_wind_speed(ws);
+        check_mass(m);
+        check_area(a);
+        check_attach_force(af);
     }
 
     
-    void checkDetachment() {
+    void check_wind_speed(float ws) {
+        if (ws >= 0) {
+            wind_speed = ws;
+        } else {
+            cout << "Wind speed can't be negative. Setting to 0." << endl;
+            wind_speed = 0;
+        }
+    }
+
+    // Method to check mass
+    void check_mass(float m) {
+        if (m >= 0) {
+            mass = m;
+        } else {
+            cout << "Mass can't be negative. Setting to 0." << endl;
+            mass = 0;
+        }
+    }
+
+    
+    void check_area(float a) {
+        if (a >= 0) {
+            area = a;
+        } else {
+            cout << "Area can't be negative. Setting to 0." << endl;
+            area = 0;
+        }
+    }
+
+   
+    void check_attach_force(float af) {
+        if (af >= 0) {
+            attach_force = af;
+        } else {
+            cout << "Attach force can't be negative. Setting to 0." << endl;
+            attach_force = 0;
+        }
+    }
+
+   
+    float calculateDragForce() const {
+        return 0.5 * Cd * density * area * wind_speed * wind_speed;
+    }
+
+   
+    void checkDetachment() const {
         float drag_force = calculateDragForce();
         float total_force = sqrt((drag_force * drag_force) + (mass * g) * (mass * g));
 
@@ -42,24 +86,25 @@ public:
 };
 
 int main() {
-float mass,area,wind_speed,attach_force;
-    cout<<"please enter the mass of the leaf\n";
-    cin>>mass;
-    cout<<"please enter the area of the leaf\n";
-    cin>>area;
-    cout<<"tell the wind speed\n";
-    cin>>wind_speed;
-    cout<<"write the attach force\n";
-    cin>>attach_force;
+    float mass, area, wind_speed, attach_force;
+    
+    cout << "Please enter the mass of the leaf: ";
+    cin >> mass;
+    cout << "Please enter the area of the leaf: ";
+    cin >> area;
+    cout << "Tell the wind speed: ";
+    cin >> wind_speed;
+    cout << "Write the attach force: ";
+    cin >> attach_force;
+    
     
     Leaf leaf(wind_speed, mass, area, attach_force);
-
     
+  
     leaf.checkDetachment();
 
     return 0;
 }
-
 
 
 
